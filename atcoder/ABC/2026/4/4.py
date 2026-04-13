4af9c894b55981e59745ddd4a37a49e479dba309
Yuta Ko <kageyu05ko@gmail.com>
2026-04-14 03:00:07 +0900

    4/14_finish_B

--- atcoder/ABC/2026/4/4.py
+++ atcoder/ABC/2026/4/4.py
@@ -1,6 +1,6 @@
 # ABC-2026-4
 
-# A
+# A - Gothec
 M,D = map(int,input().split())
 # print(M)
 # print(D)
@@ -23,4 +23,26 @@ if (M , D) in check_goseku:
   print("Yes")
 else:
   print("No")
+
+# B - Draw Frame 
+H,W = map(int,input().split())
+
+# 1行目と最終行は全て#、それ以外は最初と最後が#でそれ以外は.を出力する
+for i in range(H):
+    for j in range(W):
+        if i == 0 or i == H-1 or j == 0 or j == W-1:
+            print('#', end='')
+        else:
+            print('.', end='')
+    # 改行を入れる場所が間違ってた
+    print('')
+
+
+# 考え方はあってた、しかし、相変わらず細かい出力の書き方とかが間違っていたりend=''とかって覚えてない
+# 1行目と最終行は全て#、それ以外は最初と最後が#でそれ以外は.を出力する
+# これは自力で思いつけた
+# まだまだ力不足
+# 面白かったが
+
+# C - Fishbones
 
